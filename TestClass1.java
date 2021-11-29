f32a9ad55d0ed5ea21d9b1fe62ee8e0b5ae01ac5
Maksim Tarasov <roronoazorroippo@gmail.com>
2021-11-29 22:53:08 +0400

    upload files

--- TestClass1.java
+++ TestClass1.java
@@ -1,4 +1,6 @@
 public TestClass {
-	System.out.println("This is a test class for Git lessons")
-	System.out.println("1_Update_3")
+        public static void main(String[] args) {
+	System.out.println("This is a test class for Git lessons!");
+	System.out.println("Update_1_01");
+  }
 }
